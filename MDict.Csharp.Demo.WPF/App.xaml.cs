6980532aa3ae0eb417108eefa79a11453c9c744d
Jack251970 <1160210343@qq.com>
2025-07-04 12:58:26 +0800

    Add project files.

---
+++ MDict.Csharp.Demo.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MDict.Csharp.Demo.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
